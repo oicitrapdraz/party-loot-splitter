37ad8316d929c05ff4bac5ab9ff35797bed66152
Patricio Sard <patriciosard@gmail.com>
2021-07-15 22:50:00 -0400

    Fix RuboCop offenses

--- app/controllers/party_loot_splitter_controller.rb
+++ app/controllers/party_loot_splitter_controller.rb
@@ -1,6 +1,5 @@
 class PartyLootSplitterController < ApplicationController
-  def calculator
-  end
+  def calculator; end
 
   def result
     @transfers = SplitCalculator.new(params[:party_hunt_log]).obtain_transfers
--- app/services/split_calculator.rb
+++ app/services/split_calculator.rb
@@ -1,5 +1,5 @@
 class SplitCalculator
-  attr_reader :party_hunt_log, :number_of_players
+  attr_reader :party_hunt_log, :number_of_players, :duration, :loot_type, :balance
 
   def initialize(party_hunt_log)
     @party_hunt_log = party_hunt_log
@@ -8,28 +8,35 @@ class SplitCalculator
   def obtain_transfers
     lines = party_hunt_log.split(/\r?\n\t?/)
 
-    duration = lines.second.gsub('Session ', '')
+    set_basic_data(lines)
 
-    loot_type = lines[2]
-
-    balance = lines[5].gsub('Balance ', '').tr(',', '').to_i
-
-    players = parse_players_input(lines)
+    players = parse_players_from_input(lines)
 
     balance_per_player = (balance / players.count.to_f).floor
 
-    match_balances(balance, players)
+    { number_of_players: number_of_players, duration: duration, loot_type: loot_type, balance: balance,
+      transfers: match_balances(balance, players) }
   end
 
   private
 
-  def parse_players_input(lines)
+  def set_basic_data(lines)
+    @duration = lines.second.gsub('Session ', '')
+
+    @loot_type = lines[2]
+
+    @balance = lines[5].gsub('Balance ', '').tr(',', '').to_i
+  end
+
+  def parse_players_from_input(lines)
     players = []
 
     data_grouped_by_players = lines[6..-1].each_slice(6)
 
+    @number_of_players = data_grouped_by_players.count
+
     data_grouped_by_players.to_a.each do |raw_player_data|
-      player = { }
+      player = {}
 
       player[:name] = raw_player_data.first.gsub(' (Leader)', '')
       player[:balance] = raw_player_data.fourth.gsub(/Balance:? /, '').tr(',', '').to_i
@@ -43,21 +50,22 @@ class SplitCalculator
   def match_balances(balance, players)
     transfers = []
 
-    valid_split = [balance/ players.count + 1] * (balance% players.count) + [balance/ players.count] * (players.count - balance% players.count)
+    valid_split = [balance / players.count + 1] * (balance % players.count) + [balance / players.count] * (players.count - balance % players.count)
 
     players.each_with_index do |first_player, first_index|
-      if first_player[:balance] > valid_split[first_index]
-        players.each_with_index do |second_player, second_index|
-          if second_player[:balance] < valid_split[second_index]
-            first_player_surplus = first_player[:balance] - valid_split[first_index]
-            second_player_deficit = valid_split[second_index] - second_player[:balance]
-            transfer_amount = [first_player_surplus, second_player_deficit].min
+      next unless first_player[:balance] > valid_split[first_index]
 
-            transfers.append(from: first_player[:name], to: second_player[:name], amount: transfer_amount)
-            first_player[:balance] -= transfer_amount
-            second_player[:balance] += transfer_amount
-          end
-        end
+      players.each_with_index do |second_player, second_index|
+        next unless second_player[:balance] < valid_split[second_index]
+
+        first_player_surplus = first_player[:balance] - valid_split[first_index]
+        second_player_deficit = valid_split[second_index] - second_player[:balance]
+        transfer_amount = [first_player_surplus, second_player_deficit].min
+
+        transfers.append(from: first_player[:name], to: second_player[:name], amount: transfer_amount)
+
+        first_player[:balance] -= transfer_amount
+        second_player[:balance] += transfer_amount
       end
     end
 
